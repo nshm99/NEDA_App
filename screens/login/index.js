4328c1cbfefac8f2627c9427df4df8cc6cc533ba
nargeshm99 <nargeshm99@outlook.com>
2019-07-06 10:59:50 +0430

    Add login page
    add UI for login page

---
+++ screens/login/index.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import {
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
+
+import styles from './login.style';
+
+const background = require("./BG2.jpg");
+const lockIcon = require("./login1_lock.png");
+const personIcon = require("./login1_person.png");
+
+export default class LoginScreen extends React.Component {
+  state = {
+    UserName : "",
+    Password : "",
+  }
+
+  
+
+  render() {
+    const{UserName,Password}=this.state
+    return (
+      <View style={styles.container}>
+        <ImageBackground
+          imageStyle={{ opacity: 0.7 }}
+          source={background} 
+          style={[styles.container, styles.bg]}
+          resizeMode="cover"
+           blurRadius={1.5}
+        >
+          <View style={styles.wrapper}>
+            <View style={styles.inputWrap}>
+              <View style={styles.iconWrap}>
+                <Image source={personIcon} style={styles.icon} resizeMode="contain" />
+              </View>
+              <TextInput 
+                placeholder="نام کاربری" 
+                placeholderTextColor="#FFF"
+                style={styles.input} 
+                value = {UserName}
+                onChangeText = {(username) => this.setState({UserName : username})}
+              />
+            </View>
+
+            <View style={styles.inputWrap}>
+              <View style={styles.iconWrap}>
+                <Image source={lockIcon} style={styles.icon} resizeMode="contain" />
+              </View>
+              <TextInput 
+                placeholderTextColor="#FFF"
+                placeholder="رمز عبور" 
+                style={styles.input} 
+                secureTextEntry
+                value={Password}
+                onChangeText = {(pw) => this.setState({Password : pw})}
+              />
+            </View>
+
+            <TouchableOpacity 
+              activeOpacity={.5}
+              opacity={10000}
+            >
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>ورود</Text>
+              </View>
+            </TouchableOpacity>
+
+          </View>
+
+          <View style={styles.container}>
+            <View style={styles.signupWrap}>
+              <TouchableOpacity 
+                activeOpacity={.5} 
+                onPress = {() => this.props.navigation.navigate('SignUp')}
+              >
+                <View>
+                  <Text style={styles.signupLinkText}>ایجاد حساب جدید  </Text>
+                </View>
+              </TouchableOpacity>
+              <Text style={styles.accountText}>  حساب کاربری ندارید؟</Text>
+            </View>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
+}
+
